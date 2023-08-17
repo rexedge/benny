eda6e8ea921ea58e69159efe5bfb85b102ff0ac1
Abdullah Momoh <momohabdullah20@gmail.com>
2023-08-17 15:09:03 +0100

    updated food menu

--- data.js
+++ data.js
@@ -6,5 +6,357 @@ export const data = [
           description: '',
           image: '',
           section: 'drink'
+     },
+     {
+          name: 'Oha soup',
+          price: '1000',
+          category: 'soup',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Afang Soup',
+          price: '1000',
+          category: 'soup',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Egusi Soup',
+          price: '1000',
+          category: 'soup',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Okro Soup',
+          price: '1000',
+          category: 'soup',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Bitter Leaf',
+          price: '1000',
+          category: 'soup',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Ogbonno Soup',
+          price: '1000',
+          category: 'soup',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Vegetable Soup',
+          price: '1000',
+          category: 'soup',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Curry Sauce',
+          price: '1000',
+          category: 'sauce',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Vegetable Sauce (Ugu or Green Veg)',
+          price: '1000',
+          category: 'sauce',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Mixed Vegetable Sauce',
+          price: '1000',
+          category: 'sauce',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Egg Sauce',
+          price: '1000',
+          category: 'sauce',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Semolina',
+          price: '700',
+          category: 'swallow',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Wheat',
+          price: '700',
+          category: 'swallow',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Garri',
+          price: '700',
+          category: 'swallow',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Poundo Flour',
+          price: '700',
+          category: 'swallow',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Oat Meal',
+          price: '700',
+          category: 'swallow',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Bean Porridge',
+          price: '1000',
+          category: 'africana',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Yam Porridge',
+          price: '1500',
+          category: 'africana',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Sweet Potato Porridge',
+          price: '1500',
+          category: 'africana',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'White Yam',
+          price: '1000',
+          category: 'africana',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Assorted Goat Meat',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Beef',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Chicken',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Turkey',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Goat Meat',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Cow Leg',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Cow Tail',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Dry Fish',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Sharky',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Chicken Wings',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Protein with any of the soups',
+          price: '2500',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Protein with swallow',
+          price: '4000',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Protein with any rice',
+          price: '4000',
+          category: 'proteins',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Sweet Potatoes',
+          price: '1000',
+          category: 'sides',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'French Fries',
+          price: '1000',
+          category: 'sides',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Plaintain Fries',
+          price: '1000',
+          category: 'sides',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Yam Fries',
+          price: '1000',
+          category: 'sides',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Moi Moi',
+          price: '500',
+          category: 'sides',
+          description: '',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Chicken Salad',
+          price: '4000',
+          category: 'salad',
+          description: 'Chicken, Egg, Lettuce, Fresh Tomatoes, Cabbage, Cucumber, Sweet Corn and Salad cream',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Vegetable Salad',
+          price: '2000',
+          category: 'salad',
+          description: 'Cabbage, Cucumber, Lettuce, Baked Beans, Sweet Corn and Egg',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Benny Classic Salad',
+          price: '2500',
+          category: 'salad',
+          description: 'Mixed Vegetable White and Purple Cabbage, Cucumber, Sweet Corn, Lettuce, Egg, Macaroni, Sweet or Irish Potato, Baked Beans and Chef\'s Cream',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Tuna or Sardine Sandwich',
+          price: '3500',
+          category: 'sandwiches',
+          description: 'Lettuce, Tomatoes, 3 Slices of Toast bread, french fries and Tuna',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Club Sandwich',
+          price: '4000',
+          category: 'sandwiches',
+          description: '4 slice of toast bread, egg, chicken, french fries, lettuce, tomatoes, cheese, cucumber',
+          image: '',
+          section: 'food'
+     },
+     {
+          name: 'Meatpie',
+          price: '500',
+          category: 'snacks',
+          description: '',
+          image: '',
+          section: 'food'
      }
 ]
